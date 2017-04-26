8d36ab8f3f9dbc85c01062e53458842ba6fd9af5
Filip Delbaen <filipdelbaen@msn.com>
2017-04-26 23:30:16 +0200

    copy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 mvn clean install
-cp -v my-first/target/my-first-1.0-SNAPSHOT.jar "D:\Java\jboss-5.1.0.GA\server\default\deploy"
+
+cp -v my-first/my-first-service/target/my-first-service-1.0-SNAPSHOT.jar \
+            "D:\Java\jboss-5.1.0.GA\server\default\deploy"
